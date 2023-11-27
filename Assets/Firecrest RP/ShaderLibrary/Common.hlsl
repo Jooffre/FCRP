46c6733462935a5e3bf0d650899294f971419a1b
Joffre <138578760+Jooffre@users.noreply.github.com>
2023-11-27 21:23:33 +0800

    Reorganize program schema.

--- Assets/Firecrest RP/ShaderLibrary/Common.hlsl
+++
@@ -1,9 +0,0 @@
-// This file summarizes necessary header files used in shader
-
-#ifndef FIRECREST_COMMON_INCLUDED
-#define FIRECREST_COMMON_INCLUDED
-
-#include "Basic.hlsl"
-#include "General.hlsl"
-
-#endif
